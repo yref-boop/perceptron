668442da444633208732cd7d747e5f667b89bc94
yref-boop <regofdez.yago@gmail.com>
2022-06-16 11:29:40 +0200

    feat: added error correction code

--- ann.hs
+++ ann.hs
@@ -1,3 +1,11 @@
+µ = 1
+
+-- step 
+step x = 
+    if x < 0
+        then -1
+    else 1
+
 -- relu
 relu x = max(0,x)
 
@@ -14,7 +22,13 @@ untuple (weight, value) =
     weight*value
 
 -- neuron function
-neuron action_function inputs =
-    action_function (foldl (+) (map untuple inputs))
+neuron action_function inputs bias =
+    action_function ( bias + foldl (+) (map untuple inputs))
 
+-- delta rule
+delta_rule (weight, value) µ w d y x =
+ (w + µ * (d - y) * x, value)
+
+weight_correction inputs =
+    map delta_rule inputs
 
