50308eb897592a793c46c08c7cd731433077adf8
yref-boop <regofdez.yago@gmail.com>
2022-12-04 22:21:27 +0100

    feat: train function implemented
    learn function has been modified accordingly
    this code should be tested

--- Main.hs
+++ Main.hs
@@ -26,35 +26,40 @@ neuron activation_function inputs bias =
 
 -- delta rule
 delta_rule :: Float -> Float -> Float -> (Float, Float) -> (Float, Float)
-delta_rule n error x (weight, value) =
+delta_rule n error x (weight, value) = 
     (weight + n * error * x, value)
 
+-- get element at specific position
+data_at :: Int -> [a] -> a
+data_at _ [] = error "empty list"
+data_at y (x:xs)  | y <= 0 = x
+                 | otherwise = data_at (y-1) xs
+
+
 -- weight modification
-learn :: [(Float,Float)] -> Float -> [(Float,Float)]
-learn inputs error =
-    fmap (delta_rule 0.1 error 0.1) inputs
+learn :: [[(Float, Float)]] -> Int -> [Float] -> (Float -> Float) -> [[(Float, Float)]]
+learn inputs element expected_results activation_function = 
+    let error = ((data_at element expected_results) - (neuron activation_function (data_at element inputs) 1.0)) in
+    
+    fmap (fmap (delta_rule 0.1 error 0.1)) inputs
 
--- training function
---train_iteration inputs expected_result error_threshold =
---    let error = expected_result - (neuron sigmoid inputs 1.0) in
---    if (absolute_float error > error_threshold)
---        then train (learn inputs error) expected_result error_threshold
---    else inputs
-
-dataAt :: Int -> [a] -> a
-dataAt _ [] = error "Empty List!"
-dataAt y (x:xs)  | y <= 0 = x
-                 | otherwise = dataAt (y-1) xs
 
 -- check how bias worked
 train :: [[(Float,Float)]] -> [Float] -> Float -> Float -> Int -> Int -> (Float -> Float) -> Int -> [[Float]]
-
 train inputs expected_results error error_threshold epoch max_epoch act_function element =
     
     if (epoch > max_epoch || error < error_threshold)
-        then return (fmap (fst) (dataAt element inputs))
-
-    else train (learn (dataAt element inputs) ((dataAt element expected_results) - (neuron sigmoid (dataAt element inputs) 1.0))) expected_results ((dataAt element expected_results) - (neuron sigmoid (dataAt element inputs) 1.0)) error_threshold (epoch + 1) max_epoch act_function 0
+        then return (fmap (fst) (data_at element inputs))
+    else 
+        train 
+            (learn inputs element expected_results act_function) 
+            expected_results 
+            ((data_at element expected_results)-(neuron sigmoid (data_at element inputs) 1.0))
+            error_threshold 
+            (epoch + 1) 
+            max_epoch 
+            act_function 
+            0
 
 
 -- add (weight, value)
@@ -62,7 +67,7 @@ format_input :: Float -> (Float, Float)
 format_input value = (0, value)
 
 -- main
-main :: IO ()
+main :: IO ([[Float]])
 main = do
     
     all_data <- Reader.read_data
@@ -77,5 +82,6 @@ main = do
     let max_epoch = 1000
     let act_function = sigmoid
 
-    let final_inputs = train formatted_inputs real_out 1000 error_threshold 0 max_epoch act_function 0
-    return ()
+    let final_weights = train formatted_inputs real_out 1000 error_threshold 0 max_epoch act_function 0
+
+    return (final_weights) 
