3b266f4f7743a3327c8e601e62d6a8407d54e1d0
yref-boop <regofdez.yago@gmail.com>
2022-12-04 18:12:25 +0100

    feat: main function data preparation for new train function

--- Main.hs
+++ Main.hs
@@ -31,15 +31,43 @@ learn inputs error =
     map (delta_rule 0.1 error 0.1) inputs
 
 -- training function
-train inputs expected_result error_threshold =
-    let error = expected_result - (neuron sigmoid inputs 1.0) in
-    if (absolute_float error > error_threshold)
-        then train (learn inputs error) expected_result error_threshold
-    else inputs
+--train_iteration inputs expected_result error_threshold =
+--    let error = expected_result - (neuron sigmoid inputs 1.0) in
+--    if (absolute_float error > error_threshold)
+--        then train (learn inputs error) expected_result error_threshold
+--    else inputs
 
+dataAt :: Int -> [a] -> a
+dataAt _ [] = error "Empty List!"
+dataAt y (x:xs)  | y <= 0 = x
+                 | otherwise = dataAt (y-1) xs
+
+-- check how bias worked
+
+train inputs real_out error error_threshold epoch max_epoch act_function element =
+    if (epoch > max_epoch || error < error_threshold)
+        then return map (fst) (dataAt inputs element)
+    else return [] 
+
+-- add (weight, value)
+format_input :: Float -> (Float, Float)
+format_input value = (0, value)
 
 -- main
 main :: IO ()
 main = do
-    let inputs = Reader.read_data
-    return()
+    
+    let all_data = Reader.read_data
+ 
+    let instances = fmap length all_data
+    let dimension = fmap length (fmap tail (fmap head all_data))
+    let inputs = fmap (fmap tail) all_data
+    let formatted_inputs = fmap ( fmap (fmap format_input)) inputs
+    let real_out = fmap (fmap head) all_data
+
+    let error_threshold = 1.0
+    let max_epoch = 1000
+    let act_function = sigmoid
+
+    let final_inputs = train formatted_inputs real_out 1000 error_threshold 0 max_epoch act_function 0
+    return ()
