0e6d082af69a8133fcb91fd89cb1caddffa61b2e
yref-boop <regofdez.yago@gmail.com>
2022-12-30 17:48:07 +0100

    chore: added vector module

--- Main.hs
+++ Main.hs
@@ -1,8 +1,8 @@
 module Main (main) where
 import Normalize (normalize_data)
 import System.Random
-import Data.List
-
+import Data.List as List
+import Data.Vector.Unboxed
 
 ----- # TRAINING # -----
 
@@ -20,7 +20,7 @@ sigmoid_derivative x =
 -- neuron function (weighted sum of inputs)
 neuron :: (Float -> Float) -> [Float] -> [Float] -> Float
 neuron activation_function inputs weight=
-    activation_function (foldl (+) 0.0 (zipWith (*) inputs weight))
+    activation_function (List.foldl (+) 0.0 (List.zipWith (*) inputs weight))
 
 
 -- delta rule 
@@ -38,14 +38,14 @@ next_rng limit seed = fst(randomR (0, limit) (mkStdGen seed))
 update_weights :: [[Float]] -> [Float] -> (Float -> Float) -> Int -> [Float] -> [Float]
 update_weights training_set expected_results act_function pos weights =
     let error = (expected_results !! pos) - (neuron act_function (training_set !! pos) (weights)) in
-    zipWith (delta error) weights (training_set !! pos)
+    List.zipWith (delta error) weights (training_set !! pos)
 
 
 -- list of learning functions with each position 
 train :: [[Float]] -> [Float] -> (Float -> Float) -> Int -> Int -> [[Float] -> [Float]]
 train training_set expected_results act_function instances seed =
     let all_rng = iterate (next_rng instances) seed in
-    map (update_weights training_set expected_results act_function) all_rng
+    List.map (update_weights training_set expected_results act_function) all_rng
 
 
 
@@ -64,8 +64,8 @@ zip_fold f = aux 0
 evaluate_weight :: [[Float]] -> [Float] -> Int -> Int -> [Float]
 evaluate_weight training_set weight random_number instances =
     let training_instance = (training_set !! random_number) in 
-    let approximation = zip_fold (*) weight (tail training_instance) in 
-        head training_instance - approximation :
+    let approximation = zip_fold (*) weight (List.tail training_instance) in 
+        List.head training_instance - approximation :
         evaluate_weight training_set weight (next_rng instances random_number) instances
 
 
@@ -73,15 +73,15 @@ evaluate_weight training_set weight random_number instances =
 check_weight :: [[Float]] -> Int -> Int -> Int -> [Float] -> Float
 check_weight  training_set random_number instances iterations weight =
     let list = evaluate_weight training_set weight random_number instances in
-        abs (foldl (+) 0.0 (take iterations list)) / (fromIntegral iterations)
+        abs (List.foldl (+) 0.0 (List.take iterations list)) / (fromIntegral iterations)
 
 
 -- check_weight (weight ...) !! max_value
 select_optimal:: [[Float]] -> [[Float]] -> Int  -> Int -> Int -> Int -> (Float, [Float])
 select_optimal training_set weights random_number instances iterations max_epoch =
     let get_error = check_weight training_set random_number instances iterations in
-    let weight_error = map get_error weights in
-        minimum (zip (take max_epoch weight_error) weights)
+    let weight_error = List.map get_error weights in
+        List.minimum (List.zip (List.take max_epoch weight_error) weights)
 
 
 
@@ -90,15 +90,15 @@ select_optimal training_set weights random_number instances iterations max_epoch
 -- get random_set
 random_subset :: [[Float]] -> Int -> Float -> [Int]
 random_subset general_set random_number percentage =
-    let instances = floor (fromIntegral(length general_set) * percentage) in
-    let rng_list = iterate (next_rng (length general_set)) random_number in
-        take instances (drop 1 rng_list)
+    let instances = floor (fromIntegral(List.length general_set) * percentage) in
+    let rng_list = iterate (next_rng (List.length general_set)) random_number in
+        List.take instances (List.drop 1 rng_list)
 
 
 -- get subsets from data
 split_set :: [[Float]] -> Int -> Float -> ([[Float]], [[Float]])
 split_set general_set random_number percentage =
-    let random_set = map (general_set !!) (random_subset general_set random_number percentage)
+    let random_set = List.map (general_set !!) (random_subset general_set random_number percentage)
     in (general_set \\ random_set, random_set)
 
 
@@ -110,7 +110,7 @@ main = do
     all_data <- Normalize.normalize_data
  
     -- auxiliar data gathered from input
-    let dimension = length (tail (head all_data))
+    let dimension = List.length (List.tail (List.head all_data))
 
     -- init random numbers
     rng_seed <- newStdGen
@@ -120,20 +120,20 @@ main = do
     let (training_data, auxiliar_set) = split_set all_data random_seed 0.1
     let (validation_set, test_set) = split_set auxiliar_set random_seed 0.5
 
-    print (length all_data)
-    print (length training_data)
-    print (length validation_set)
-    print (length test_set)
+    print (List.length all_data)
+    print (List.length training_data)
+    print (List.length validation_set)
+    print (List.length test_set)
 
     -- new random numbers
     rng_seed <- newStdGen
-    let random_number = next_rng (length training_data) random_seed
+    let random_number = next_rng (List.length training_data) random_seed
 
     -- training inputs calculations
-    let training_set = fmap (1:) (fmap tail training_data)
-    let int_weights = take (dimension + 1) (iterate (next_rng (length training_set)) random_number)
-    let weights = map ((/1000).fromIntegral) int_weights
-    let real_out = fmap (head) all_data
+    let training_set = fmap (1:) (fmap List.tail training_data)
+    let int_weights = List.take (dimension + 1) (iterate (next_rng (List.length training_set)) random_number)
+    let weights = List.map ((/1000).fromIntegral) int_weights
+    let real_out = fmap (List.head) all_data
 
     -- hard-coded training values
     let error_threshold = 0.001
@@ -141,14 +141,14 @@ main = do
     let act_function = sigmoid
 
     -- get all train functions
-    let train_functions = train training_set real_out act_function (length training_set) random_number
+    let train_functions = train training_set real_out act_function (List.length training_set) random_number
 
     -- apply train functions & print results
-    let trained_weights = scanl' (\x f -> f x) weights train_functions
+    let trained_weights = List.scanl' (\x f -> f x) weights train_functions
 
     -- get optimal value
     let checks_num = 5
-    let optimal_weight = select_optimal trained_weights trained_weights random_number (length training_set) checks_num max_epoch
+    let optimal_weight = select_optimal trained_weights trained_weights random_number (List.length training_set) checks_num max_epoch
 
     -- print & return value
     print optimal_weight
