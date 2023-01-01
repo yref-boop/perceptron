e7a2e13ced4178c522b7675057f4f6796399c9b0
yref-boop <regofdez.yago@gmail.com>
2023-01-01 18:40:04 +0100

    feat: training now works with vectors

--- Main.hs
+++ Main.hs
@@ -20,9 +20,9 @@ sigmoid_derivative x =
 
 
 -- neuron function (weighted sum of inputs)
-neuron :: (Float -> Float) -> [Float] -> [Float] -> Float
+neuron :: (Float -> Float) -> Vector Float -> Vector Float -> Float
 neuron activation_function inputs weight=
-    activation_function (List.foldl (+) 0.0 (List.zipWith (*) inputs weight))
+    activation_function (V.foldl (+) 0.0 (V.zipWith (*) inputs weight))
 
 
 -- delta rule 
@@ -37,14 +37,14 @@ next_rng limit seed = fst(randomR (0, limit) (mkStdGen seed))
 
 
 -- iterate learn function over weights using different examples each time
-update_weights :: Vector (Vector Float) -> Vector Float -> (Float -> Float) -> Int -> [Float] -> [Float]
+update_weights :: Vector (Vector Float) -> Vector Float -> (Float -> Float) -> Int -> Vector Float -> Vector Float
 update_weights training_set expected_results act_function pos weights =
     let error = (expected_results ! pos) - (neuron act_function (training_set ! pos) (weights)) in
-    List.zipWith (delta error) weights (training_set ! pos)
+    V.zipWith (delta error) weights (training_set ! pos)
 
 
 -- list of learning functions with each position 
-train :: Vector (Vector Float) -> Vector Float -> (Float -> Float) -> Int -> Int -> [[Float] -> [Float]]
+train :: Vector (Vector Float) -> Vector Float -> (Float -> Float) -> Int -> Int -> [Vector Float -> Vector Float]
 train training_set expected_results act_function instances seed =
     let all_rng = iterate (next_rng instances) seed in
     List.map (update_weights training_set expected_results act_function) all_rng
@@ -123,7 +123,7 @@ split_data general_vector random_number percentage =
 
 
 -- main
-main :: IO (Float,[Float])
+main :: IO (Vector Float)
 main = do
     
     -- store normalized data from data.txt
@@ -133,18 +133,15 @@ main = do
     -- auxiliar data gathered from input
     let dimension = V.length (V.tail (V.head data_vector))
 
-    -- init random numbers
+    -- get training data
     rng_seed <- newStdGen
     let (random_seed, seed) = randomR (0, V.length data_vector) (rng_seed)
-
     let (training_data, aux_vector) = split_data data_vector random_seed 0.9
 
-    -- new random numbers
+    -- get validation & test sets
     rng_seed <- newStdGen
     let random_number = next_rng (List.length training_data) random_seed
-
     let (validation_set, test_set) = split_data aux_vector random_seed 0.5
-
 
     -- training inputs calculations
     let training_vector = fmap (flip update (V.singleton (0,1.0))) training_data
@@ -154,19 +151,18 @@ main = do
 
     -- hard-coded training values
     let error_threshold = 0.001
-    let max_epoch = 1000000
+    let max_epoch = 10000000
     let act_function = sigmoid
 
-    -- get all train functions
+    -- get & apply all train functions
     let train_functions = train training_vector real_out act_function (V.length training_vector) random_number
-
-    return (1,[1,2])
-
-    {- 
-
-    -- apply train functions & print results
     let trained_weights = List.scanl' (\x f -> f x) weights train_functions
 
+
+    print (trained_weights !! max_epoch)
+    return (trained_weights !! max_epoch)
+    
+    {- 
     -- get optimal value
     let checks_num = 5
     let optimal_weight = select_optimal trained_weights trained_weights random_number (List.length training_set) checks_num max_epoch
